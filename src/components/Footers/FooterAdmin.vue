bfe0f9c3a64c6c81dde70607ed5925eea992e291
harkiramadhan <harkiramadhan@gmail.com>
2020-10-23 01:37:56 +0700

    update

--- src/components/Footers/FooterAdmin.vue
+++ src/components/Footers/FooterAdmin.vue
@@ -1,7 +1,6 @@
 <template>
 <footer class="block py-4">
     <div class="">
-        <hr class="mb-4 border-b-1 border-gray-300" />
         <div class="flex flex-wrap items-center md:justify-between justify-center">
             <div class="w-full md:w-4/12 px-4">
                 <div class="text-sm text-gray-600 font-semibold py-1 text-center md:text-left">
